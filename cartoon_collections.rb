419cd68cc8a09f1df99cc2a7f3b07a4bce5bfef5
Injae Lee <ilee.injae@gmail.com>
2020-04-08 23:18:58 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -18,7 +18,7 @@ def summon_captain_planet(planeteer_calls)
   # Return a new array of the results
   
   #planeteer_calls.map { |element| "#{element.capitalize}!" }
-  
+  planeteer_calls.map { |element| element.capitalize + "!" } 
 end
 
 def long_planeteer_calls(planeteer_calls)
