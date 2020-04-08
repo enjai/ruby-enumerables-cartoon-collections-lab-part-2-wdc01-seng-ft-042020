1f146a6970fa871e99bf86625cc66ba1bdd91292
Injae Lee <ilee.injae@gmail.com>
2020-04-08 23:18:14 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -17,7 +17,7 @@ def summon_captain_planet(planeteer_calls)
   # Use an Enumerable to capitalize and add '!' to every element in the passed in array
   # Return a new array of the results
   
-  planeteer_calls.map { |element| "#{element.capitalize}!" }
+  #planeteer_calls.map { |element| "#{element.capitalize}!" }
   
 end
 
@@ -42,6 +42,7 @@ end
 
 def find_valid_calls
   valid_calls = ["Earth!", "Wind!", "Fire!", "Water!", "Heart!"]
-  planeteer_calls.find {|element| valid_calls.include?(element)}
+  
+  planeteer_calls.find { |element| valid_calls.include?(element) }
 end
 
